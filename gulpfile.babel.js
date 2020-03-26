b220cb975a016d4780ffebc30a8105936981aca5
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-03-26 20:44:32 +0900

    scss, img adding using babel&gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,25 @@ import gulp from "gulp";
 import gpug from "gulp-pug"
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
+import sass from "gulp-sass";
+
+sass.compiler = require('node-sass');
 
 const routes = {
     pug: {
         watch: "src/**/*.pug",
         src: "src/*.pug", // 하위 폴더 안까지. **/*.pug,
         dest: "dest"
+    },
+    img: {
+        src: "src/img/*",
+        dest: "dest/img"   
+    },
+    css: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/styles.scss",
+        dest: "dest/css"
     }
 }
 
@@ -20,10 +33,21 @@ const webserver = () => gulp.src("dest").pipe(ws({livereload: true, open: true})
 
 const watch = () => {
     gulp.watch (routes.pug.watch, pug);
+    gulp.watch (routes.img.src, img);
+    gulp.watch (routes.css.watch, styles);
 };
 
-const prepare = gulp.series([clean]);
-const assets = gulp.series([pug]);
+const img = () => gulp
+                    .src(routes.img.src)
+                    .pipe(image())
+                    .pipe(gulp.dest(routes.img.dest));
+const styles = () => gulp
+                    .src(routes.css.src)
+                    .pipe(sass().on('error', sass.logError))
+                    .pipe(gulp.dest(routes.css.dest));
+
+const prepare = gulp.series([clean, img]);
+const assets = gulp.series([pug, styles]);
 const postDev = gulp.parallel([webserver, watch]);
 
 
