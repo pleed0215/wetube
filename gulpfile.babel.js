995cbe7ef70bf3bccdc585ade1b936bac25d1719
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-03-27 12:30:19 +0900

    GULP initializing job's completed

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,12 @@ import gulp from "gulp";
 import gpug from "gulp-pug"
 import del from "del";
 import ws from "gulp-webserver";
-import image from "gulp-image";
-import sass from "gulp-sass";
-import autop from "gulp-autoprefixer";
-import csso from "gulp-csso";
-import browserfy from "gulp-bro";
-import babelify from "babelify";
+import image from "gulp-image"; // 이미지 최적화를 위한 gulp
+import sass from "gulp-sass"; // sass를 컴파일 위해.
+import autop from "gulp-autoprefixer"; // css를 여러 브라우저에서 사용할 수 있게끔 만들어준다.
+import csso from "gulp-csso"; // css를 한 줄로 .. 
+import browserify from "gulp-bro"; // browserify 사용을 위함. import 같은 문법을 브라우저는 이해를 못하므로.. 사용하는 것이라 한다.
+import babelify from "babelify"; // babelify 
 
 sass.compiler = require('node-sass');
 
@@ -28,7 +28,7 @@ const routes = {
     },
     js: {
         watch: "src/js/**/*.js",
-        src: "src/js/*",
+        src: "src/js/main.js",
         dest: "dest/js"
     }
 }
@@ -44,6 +44,7 @@ const watch = () => {
     gulp.watch (routes.pug.watch, pug);
     gulp.watch (routes.img.src, img);
     gulp.watch (routes.css.watch, styles);
+    gulp.watch (routes.js.watch, js);
 };
 
 const img = () => gulp
@@ -62,10 +63,16 @@ const styles = () => gulp
                     .pipe(csso())
                     .pipe(gulp.dest(routes.css.dest));
 const js = () => gulp.src (routes.js.src)
-                     .pipe
+                     .pipe(browserify({
+                        transform: [
+                            babelify.configure ( {presets: ['@babel/preset-env']}),
+                            ['uglifyify', {global: true} ]
+                        ]
+                     } ))
+                     .pipe (gulp.dest (routes.js.dest));
 
 const prepare = gulp.series([clean, img]);
-const assets = gulp.series([pug, styles]);
+const assets = gulp.series([pug, styles, js]);
 const postDev = gulp.parallel([webserver, watch]);
 
 
